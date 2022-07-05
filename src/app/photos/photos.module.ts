c34442a2aed2e6e1061961d5acceeac1f1330f2b
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-07-05 12:16:33 -0300

    pipe e implementação , ngif e mais..

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,16 +6,19 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent, 
-        PhotoListComponent, 
-        PhotoFormComponent, PhotosComponent 
+    declarations: [
+        PhotoComponent,
+        PhotoListComponent,
+        PhotoFormComponent,
+        PhotosComponent,
+        FilterByDescription
     ],
-    imports: [ 
+    imports: [
         HttpClientModule,
-        CommonModule 
+        CommonModule
     ]
 })
 export class PhotosModule {}
