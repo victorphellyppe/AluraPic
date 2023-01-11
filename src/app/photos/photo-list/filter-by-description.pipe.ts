7fbffe700d0bffc1ac155c67f30aad3e763c9cd0
Victor Oliveira <victor.oliveira@blueservice.com.br>
2023-01-11 12:29:01 -0300

    resto commit

--- src/app/photos/photo-list/filter-by-description.pipe.ts
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -1,17 +1,22 @@
-import { Pipe, PipeTransform } from "@angular/core";
-import { Photo } from "../photo/photo";
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: "filterByDescription" })
+import { Photo } from '../photo/photo';
+
+@Pipe({ name: 'filterByDescription'})
 export class FilterByDescription implements PipeTransform {
-  transform(photos: Photo[], descriptionQuery: string) {
-    descriptionQuery = descriptionQuery.trim().toLowerCase();
 
-    if (descriptionQuery) {
-      return photos.filter((photo) =>
-        photo.description.toLowerCase().includes(descriptionQuery)
-      );
-    } else {
-      return photos;
+    transform(photos: Photo[], descriptionQuery: string) {
+        descriptionQuery = descriptionQuery
+            .trim()
+            .toLowerCase();
+
+        if(descriptionQuery) {
+            return photos.filter(photo => 
+                photo.description.toLowerCase().includes(descriptionQuery)
+            );
+        } else {
+            return photos;
+        }
     }
-  }
+
 }
